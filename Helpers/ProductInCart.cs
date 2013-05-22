213f707fb393d2047804cdb42274876ca114584f
Ilya <bkmz2005@yandex.ru>
2013-05-22 12:42:33 +0300

    create cart interface

---
+++ Helpers/ProductInCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public class ProductInCart
+    {
+        public int Id;
+        public string Name;
+        public int CategoryId;
+        public double PriceOfOneItem;
+        public string SellByWeight;
+        public double AverageWeight;
+        public string UnitOfMeasure;
+        public int Count;
+        public double TotalPrice;
+    }
+}
---
+++ Interfaces/ICart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace Interfaces
+{
+    interface ICart
+    {
+        void Add(string UserEmail, int ProductId, int Count);
+        List<Helpers.ProductInCart> GetAllChart(string UserEmail);
+        void DeleteProduct(string UserEmail, int ProductId);
+        void Clear(string UserEmail);
+        void UpateCount(string UserEmail, int NewCount);
+        double GetTotalPrice(string UserEmail);
+        
+        //Other methods be realize in future
+        //void Buy(string UserEmail);
+    }
+}
