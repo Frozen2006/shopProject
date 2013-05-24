abf6c3be4e6fdaeb21a9f96a7dcc0ed6082da25b
Ilya <bkmz2005@yandex.ru>
2013-05-24 09:55:08 +0300

    Add ImageController.cs

---
+++ TestProject/Controllers/ImageController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class ImageController : Controller
+    {
+        //
+        // GET: /Image/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryImg(int id)
+        {
+            string FilePath = Server.MapPath("~/Content/images/categories/" + Convert.ToString(id) + ".jpg");
+
+            if (System.IO.File.Exists(FilePath))
+            {
+                return File(FilePath, "image");
+            }
+            else
+            {
+                return File(Server.MapPath("~/Content/images/categories/NoImage.jpg"), "Image");
+            }
+            
+        }
+
+        public ActionResult ProductImg(int id)
+        {
+            string FilePath = Server.MapPath("~/Content/images/products/" + Convert.ToString(id) + ".jpg");
+
+            if (System.IO.File.Exists(FilePath))
+            {
+                return File(FilePath, "image");
+            }
+            else
+            {
+                return File(Server.MapPath("~/Content/images/products/NoImage.jpg"), "Image");
+            }
+
+        }
+
+    }
+}
