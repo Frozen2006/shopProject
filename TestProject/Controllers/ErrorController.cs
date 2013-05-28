8ea4b13103c0807d7b32ec9cdc85d0d959132d40
AntonNarkevich <17140th@gmail.com>
2013-05-28 14:33:50 +0300

    Links fixed

---
+++ TestProject/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Unknown()
+        {
+            return View("Error");
+        }
+        public ViewResult NoAccess()
+        {
+            //Response.StatusCode = 404;  //you may want to set this to 200
+            return View("NoAccess");
+        }
+        public ViewResult NotFound()
+        {
+            return View("NotFound");
+        }
+    }
+}
