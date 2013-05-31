43c7147619d4e52a350beccad8bd0b2d67699c3f
AntonNarkevich <17140th@gmail.com>
2013-05-31 19:19:11 +0300

    refactored

---
+++ TestProject/Content/js/productpage.js
@@ -0,0 +1,201 @@
+﻿function getPage() {
+
+    var params = {
+        categoryId: "@Model.Category.Id",
+        sort: "@Model.SortType",
+        pageSize: "@Model.PageSize",
+        reverse: "@Model.Reverse"
+    }
+
+    var select = document.getElementById("sort");
+
+    switch (select.selectedIndex) {
+        case 0:
+            params.sort = "Alphabetic";
+            params.reverse = "False";
+            break;
+        case 1:
+            params.sort = "Alphabetic";
+            params.reverse = "True";
+            break;
+        case 2:
+            params.sort = "Price";
+            params.reverse = "False";
+            break;
+        case 3:
+            params.sort = "Price";
+            params.reverse = "True";
+            break;
+    }
+
+    window.location.href = "/product/list?" + $.param(params);
+}
+
+//Slider scripts
+    function constructSlider(sliderClass, step) {
+        $("." + sliderClass).slider({
+            range: "min",
+            min: 0,
+            max: 10,
+            value: 0,
+            step: step,
+            slide: function (event, ui) {
+                var counter = getCounter(event.target);
+                counter.value = ui.value;
+
+                $(counter).change();
+            }
+        });
+    }
+
+    $(function () {
+        constructSlider("int_slider", 1);
+        constructSlider("float_slider", 0.1);
+    });
+
+function countInput(event, price, sliderClass)
+{
+    slider = getSlider(event.target);           
+
+    var stringCount = event.target.value;
+
+    //Parsing input value. 
+    var count;
+    if (sliderClass == "int_slider") {
+        count = parseInt(stringCount) || 0;
+    }
+    else {
+        count = parseFloat(stringCount.replace(",", ".")) || 0;
+    }
+
+    //If can't parse than set it to 0.
+    //event.target.value = count;
+        
+        
+    $(slider).slider("value", count);
+
+    //Set estimated price.
+    var pricespan = getPrice(event.target);
+    pricespan.innerHTML = (count * price).toFixed(2);
+}
+
+function correctInput(event, sliderClass) {
+    var stringCount = event.target.value;
+
+    //Trying to parse input
+    var count;
+    if (sliderClass == "int_slider") {
+        count = parseInt(stringCount) || 0;
+    }
+    else {
+        count = parseFloat(stringCount.replace(",", ".")).toFixed(1) || 0;
+    }
+
+    //If can't parse than set it to 0.
+    event.target.value = count;
+}
+
+//Returns counter element by its sibling node (e.g. button event sender)
+function getCounter(element) {
+    return element.parentNode.getElementsByClassName("counter")[0];
+}
+
+//Returns input hidden element with product id by its sibling node (e.g. button event sender)
+function getIdInput(element) {
+    return element.parentNode.getElementsByClassName("product_id")[0];
+}
+
+//Returns slider by its sibling node (e.g. button event sender)
+function getSlider(element) {
+    return element.parentNode.getElementsByClassName("slider")[0];
+}
+
+//Returns price span by its sibling node (e.g. button event sender)
+function getPrice(element) {
+    return element.parentNode.getElementsByClassName("estimated_price_value")[0];
+}
+
+//Adding to cart scripts
+
+    //public ActionResult AddToCart(int productId, int count)
+    function sendRequest(id, c) {
+        $.ajax
+        ({
+            url: "/Product/AddToCart",
+            data: { productId: id, count: c.toString().replace(".",",") },
+            type: "POST",
+            success: function (data) {
+                newAlert('alert', data);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                newAlert('alert alert-error', errorThrown);
+            }
+        })
+    }
+
+function addToCart(event) {
+    var input = getIdInput(event.target);
+    var id = parseInt(input.value);
+
+    var counter = getCounter(event.target);
+    var count = parseFloat(counter.value);
+
+    if (count && count > 0) {
+        sendRequest(id, count);
+    }
+}
+
+//creating an area for alerts
+
+$(function () {
+    $("body").append("<div id='alert_area'></div>");
+});
+    
+function newAlert(type, message) {
+    $("#alert_area").append($("<div class='alert-message " + type + " fade in' data-alert><p> " + message + " </p></div>"));
+    $(".alert-message").delay(2000).fadeOut("slow", function () { $(this).remove(); });
+}
+
+function addAll() {
+
+    var productArray = new Array();
+    var countArray = new Array();
+
+    var idInputs = document.getElementsByClassName("product_id");
+
+    for (var i = 0; i < idInputs.length; i++) {
+        var count = getCounter(idInputs[i]);
+    
+        var numCount = parseFloat(count.value) || 0;
+
+        if (numCount > 0) {
+            productArray.push(idInputs[i].value);
+            countArray.push(numCount.toString().replace(".", ","));
+        }
+    }
+
+    if (productArray.length != 0
+        && countArray.length != 0
+        && productArray.length == countArray.length) {
+
+        var postData = {
+            productIds: productArray,
+            counts: countArray
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/Product/AddArrayToCart",
+            data: postData,
+            success: function (data) {
+                newAlert('alert', data.report);
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                newAlert('alert alert-error', XMLHttpRequest.responseText);
+            },
+            dataType: "json",
+            traditional: true
+        });
+    }
+
+}
--- TestProject/Controllers/ProductController.cs
+++ TestProject/Controllers/ProductController.cs
@@ -40,32 +40,33 @@ namespace TestProject.Controllers
 
         public ActionResult AddToCart(string productId, string count)
         {
+            string email = _userService.GetEmailIfLoginIn();
+            if (email == null)
+            {
+                Response.StatusCode = 403;
+                return Content("User is not logged in");
+            }
+
             int intProductId;
             double doubleCount;
 
-            if (!int.TryParse(productId, out intProductId) ||
-                !double.TryParse(count, out doubleCount))
+            try
             {
-                return Content("Incorrect arguments");
+                intProductId = int.Parse(productId);
+                doubleCount = double.Parse(count);
             }
-
-            var cookie = Request.Cookies["session_data"];
-            if (cookie == null)
+            catch (Exception ex)
             {
-                return Content("User is not logged in");
+                Response.StatusCode = 400;
+                return Content("Incorrect arguments:<br>" + ex.Message);
             }
-
-            string email = _userService.GetUserEmailFromSession(cookie.Value);
-            if (email == null)
-            {
-                return Content("User is not logged in");
-            }
-
 
             Product product = _prodService.GetProduct(intProductId);
             if (product == null)
+            {
+                Response.StatusCode = 404;
                 return Content("Product not found");
-
+            }
 
             _cartService.Add(email, intProductId, doubleCount);
             return Content(doubleCount + " units of" + product.Name + "were successfully added");
@@ -83,62 +84,49 @@ namespace TestProject.Controllers
 
         public ActionResult AddArrayToCart(string[] productIds, string[] counts)        
         {
-            Response.StatusCode = 500;
-            if (productIds == null || counts == null
-                || productIds.Length != counts.Length)
-            {
-                return Content("Incorrect arguments");
-            }
-
-            var cookie = Request.Cookies["session_data"];
-            if (cookie == null)
-            {
-                return Content("User is not logged in");
-            }
-
-            string email = _userService.GetUserEmailFromSession(cookie.Value);
+            string email = _userService.GetEmailIfLoginIn();            
             if (email == null)
             {
+                Response.StatusCode = 403;
                 return Content("User is not logged in");
             }
-
-            int[] ids;
+            
+            Product[] products;
             double[] doubleCounts;
 
             try
             {
-                ids = productIds.Select(p => int.Parse(p)).ToArray();
-                doubleCounts = counts.Select(c => double.Parse(c)).ToArray();
+                products = productIds.Select(id => int.Parse(id))
+                    .Select(id => _prodService.GetProduct(id))
+                    .ToArray();
+
+                doubleCounts = counts.Select(c => double.Parse(c))
+                    .ToArray();
             }
-            catch
+            catch (Exception ex)
             {
-                return Content("Can't parse arguments");
+                Response.StatusCode = 400;
+                return Content("Incorrect arguments:<br>" + ex.Message);
             }
-
-            var products = ids.Select(id => _prodService.GetProduct(id)).ToArray();
-
+            
             if (products.Contains(null))
-                return Content("Some products not found");
-
+            {
+                Response.StatusCode = 404;
+                return Content("Some products were not found");
+            }
 
             StringBuilder respons = new StringBuilder();
-
             for (int i = 0; i < products.Length; i++)
             {
-                _cartService.Add(email, ids[i], doubleCounts[i]);
+                _cartService.Add(email, products[i].Id, doubleCounts[i]);
+
                 respons.Append(string.Format("{0} units of {1},<br>",
                     doubleCounts[i], products[i].Name));
             }
 
             respons.Append("were successfully added!");
-            Response.StatusCode = 200;
 
-            return GetJsonReport(respons.ToString());
-        }
-
-        private JsonResult GetJsonReport(string st)
-        {
-            return Json(new { report = st });
+            return Json(new { report = respons.ToString() });
         }
     }
 }
