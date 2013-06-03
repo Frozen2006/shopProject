7e27981d2f16b390d1703ebdb2e3b850c2829e1d
AntonNarkevich <17140th@gmail.com>
2013-06-03 10:25:29 +0300

    Payment page was built

---
+++ TestProject/Controllers/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Pay()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Pay(PaymentModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //payment to database
+                //redirece to somewhere
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ TestProject/Models/PaymentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class PaymentModel
+    {
+        [Required(ErrorMessage = "Card Number is required")]
+        [RegularExpression("^4[0-9]{12}(?:[0-9]{3})?$", ErrorMessage = "Invalid card number")]
+        [DisplayName("Card number")]
+        public string CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Expiration month is required")]
+        [Range(1, 12, ErrorMessage = "Month must be between 1 and 12")]
+        [DisplayName("Card expiration month")]
+        public int ExpirationMonth { get; set; }
+
+        [Required(ErrorMessage = "Expiration day is required")]
+        [Range(1, 31, ErrorMessage = "Day must be between 1 and 31")]
+        [DisplayName("Expiration day")]
+        public int ExpirationDay { get; set; }
+
+        [Required(ErrorMessage = "Security code is required")]
+        [Range(100, 999, ErrorMessage = "Security code must be between 100 and 999")]
+        [DisplayName("Security code")]
+        public int SecurityCode { get; set; }
+
+        [DisplayName("Delivery comments")]
+        public string DeliveryComment { get; set; }
+    }
+}
