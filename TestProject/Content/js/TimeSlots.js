5e23a626176586db20bceaa2fbf9feb4321ca4dc
Ilya <bkmz2005@yandex.ru>
2013-06-03 09:47:07 +0300

    TimeSlots complete

--- TestProject/Content/js/TimeSlots.js
+++ TestProject/Content/js/TimeSlots.js
@@ -1,6 +1,6 @@
 ﻿function Bookin(hour, day, mounth, year, slotType) {
     $("#myModal").find("#bookThis")[0].onclick = function () { SendBook(hour, day, mounth, year, slotType); };
-    $("#time-place")[0].innerHTML = hour + ":00-" + (hour + 1) + ":00 " + mounth + "/" + day + "/" + year;
+    $("#time-place")[0].innerHTML = hour + ":00-" + (hour + slotType) + ":00 " + mounth + "/" + day + "/" + year;
     $("#myModal").modal('show');
 }
 
@@ -15,7 +15,7 @@ function SendBook(hour, day, mounth, year, slotType) {
         data: { hour: hour, day: day, mounth: mounth, year: year, slotType: slotType },
         type: "POST",
         success: function (data) {
-            $("td#" + hour + "-" + day + "-" + mounth + "-" + year)[0].style.background = "green";
+            $("td#" + hour + "-" + day + "-" + mounth + "-" + year + "-" + slotType)[0].style.background = "green";
             $("#myModal").modal('hide');
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
---
+++ TestProject/Models/TimeSlotsTableModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Helpers;
+
+namespace TestProject.Models
+{
+    public class TimeSlotsTableModel
+    {
+        public DateTime Today;
+        public List<DateTime> startDay;
+        public List<BookinSlot> Slots;
+        public int Step = 1;
+    }
+}
