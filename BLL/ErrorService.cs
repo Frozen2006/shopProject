bbee8911d991b1c386e2b3c6affa8ea2c0478115
AntonNarkevich <17140th@gmail.com>
2013-06-06 18:53:47 +0300

    files added

---
+++ BLL/ErrorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace BLL
+{
+    public class ErrorService : IErrorService
+    {
+        public string GetErrorDescription(ErrorCode code)
+        {
+            switch (code)
+            {
+                case ErrorCode.NotLoggedIn:
+                    return "User is not logged in";
+                case ErrorCode.NotFound:
+                    return "Resource was not found";
+                case ErrorCode.Forbidden:
+                    return "Access forbidden";
+                default:
+                    return "Unknown error occured";
+            }
+        }
+    }
+}
---
+++ Interfaces/IZipCodeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IZipCodeService
+    {
+        string GetFirstCityPartical(string particalZip);
+        List<Tuple<string,string>> GetCities(string particalZip);
+    }
+
+}
