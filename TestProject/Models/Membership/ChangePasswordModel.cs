b5d0bffd5cd9f9553666b25862c315d480732c14
Ilya <bkmz2005@yandex.ru>
2013-06-13 14:45:09 +0300

    setting's bug fixed

--- TestProject/Models/Membership/ChangePasswordModel.cs
+++ TestProject/Models/Membership/ChangePasswordModel.cs
@@ -2,23 +2,24 @@
 
 namespace iTechArt.Shop.Web.Models
 {
-    public class ChangePasswordModel
-    {
-        [DataType(DataType.Password)]
-        [Display(Name = "Old Password")]
-        public string OldPassword { get; set; }
+	public class ChangePasswordModel
+	{
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Old Password")]
+		public string OldPassword { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "Value \"{0}\" must have more then {2} chars.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
-        public string Password { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "Value \"{0}\" must have more then {2} chars.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Display(Name = "New password")]
+		public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("Password", ErrorMessage = "Password and confirm password values is not equal!")]
-        public string ConfirmPassword { get; set; }
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm new password")]
+		[Compare("Password", ErrorMessage = "Password and confirm password values is not equal!")]
+		public string ConfirmPassword { get; set; }
 
-        public bool IsSuccess { get; set; }
-    }
+		public bool IsSuccess { get; set; }
+	}
 }
